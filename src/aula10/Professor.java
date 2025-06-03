7b434b7dcbdba9810d731d58f1885083cf9da577
flourenco-daniel <danielferreiralourenco95@gmail.com>
2025-06-03 08:49:05 -0300

    subindo professor

--- src/aula10/Professor.java
+++ src/aula10/Professor.java
@@ -4,8 +4,8 @@ public class Professor extends Pessoa{
     private String especialidade;
     private float salario;
 
-    public void receberAumento(){
-
+    public void receberAumento(float aumento){
+        this.setSalario(this.getSalario() + aumento);
     }
 
     public String getEspecialidade(){
@@ -20,7 +20,7 @@ public class Professor extends Pessoa{
         return this.salario;
     }
 
-    public void setSalario(int salario){
+    public void setSalario(float salario){
         this.salario = salario;
     }
     
