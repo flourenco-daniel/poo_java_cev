506cbb71da9e49677315332066c399bc67f1fc94
flourenco-daniel <danielferreiralourenco95@gmail.com>
2025-06-05 21:41:53 -0300

    registrando técnico

---
+++ src/aula11/Tecnico.java
@@ -0,0 +1,19 @@
+package aula11;
+
+public class Tecnico extends Aluno {
+    
+    private int registroProfissional;
+    
+    public void praticar(){
+
+    }
+
+    public int getRegistroProfissional(){
+        return this.registroProfissional;
+    }
+
+    public void setRegistroProfissional(int registro){
+        this.registroProfissional = registro;
+    }
+    
+}
